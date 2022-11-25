572fe04b7c020385ad93de929deca4a1a35e9594
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-25 16:08:44 +0100

    zrobiona lista switch

--- app/src/main/java/com/example/zadanie_switch/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_switch/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.zadanie_switch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ListaSwitchy: List<Switch> = listOf(
+            findViewById<Switch>(R.id.liczba1),
+            findViewById<Switch>(R.id.liczba2),
+            findViewById<Switch>(R.id.liczba3),
+            findViewById<Switch>(R.id.liczba4),
+            findViewById<Switch>(R.id.liczba5),
+            findViewById<Switch>(R.id.liczba6),
+            findViewById<Switch>(R.id.liczba7),
+            findViewById<Switch>(R.id.liczba8),
+            findViewById<Switch>(R.id.liczba9)
+        )
+
     }
 }
