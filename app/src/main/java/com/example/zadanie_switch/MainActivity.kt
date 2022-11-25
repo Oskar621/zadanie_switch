f9dfe0437645e202e095c2e998919f85fa5eb2fa
ogkamilek <kamil.kozlowski.hel@gmail.com>
2022-11-25 16:14:05 +0100

    przycisk losowanie zdefiniowany w kodzie + zrobiony do niego kod

--- app/src/main/java/com/example/zadanie_switch/MainActivity.kt
+++ app/src/main/java/com/example/zadanie_switch/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.zadanie_switch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Switch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val sortuj_butt = findViewById<Button>(R.id.Sortuj)
+        val losuj_butt = findViewById<Button>(R.id.Losuj)
         val ListaSwitchy: List<Switch> = listOf(
             findViewById<Switch>(R.id.liczba1),
             findViewById<Switch>(R.id.liczba2),
@@ -20,6 +24,16 @@ class MainActivity : AppCompatActivity() {
             findViewById<Switch>(R.id.liczba8),
             findViewById<Switch>(R.id.liczba9)
         )
+
+        losuj_butt.setOnClickListener {
+            for(i in 0..8)
+            {
+                val liczba = Random.nextInt(0,100)
+                 ListaSwitchy[i].text = liczba.toString()
+            }
+
+
+        }
 
     }
 }
